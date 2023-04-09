5428c00c957d871fb44d116fed8bf02c6a89f38a
Your Name <you@example.com>
2023-04-09 20:40:52 +0200

    FileChooser 23

--- src/main/java/org/image/ImageProcessor.java
+++ src/main/java/org/image/ImageProcessor.java
@@ -1,17 +1,14 @@
 package org.image;
 
-import java.awt.GridLayout;
+import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import javax.imageio.ImageIO;
-import javax.swing.ImageIcon;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-import javax.swing.SwingUtilities;
-
 import javax.swing.*;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class ImageProcessor {
 
@@ -22,6 +19,7 @@ public class ImageProcessor {
         fileChooser.setAcceptAllFileFilterUsed(false);
 
         int returnValue = fileChooser.showOpenDialog(null);
+
         if (returnValue == JFileChooser.APPROVE_OPTION) {
             File selectedFile = fileChooser.getSelectedFile();
             try {
@@ -29,10 +27,12 @@ public class ImageProcessor {
                 String outputImagePath = inputImagePath.substring(0, inputImagePath.lastIndexOf('.')) + "_bw.jpg";
 
                 BufferedImage colorImage = ImageIO.read(new File(inputImagePath));
-                BufferedImage blackWhiteImage = convertToBlackAndWhite(colorImage);
-                ImageIO.write(blackWhiteImage, "jpg", new File(outputImagePath));
+                BufferedImage bwImage1 = convertToBlackAndWhite1(colorImage);
+                BufferedImage bwImage2 = convertToBlackAndWhite2(colorImage);
+                BufferedImage bwImage3 = convertToBlackAndWhite3(colorImage);
+                ImageIO.write(bwImage1, "jpg", new File(outputImagePath));
 
-                displayImages(colorImage, blackWhiteImage);
+                displayImages(colorImage, bwImage1, bwImage2, bwImage3);
             } catch (IOException e) {
                 e.printStackTrace();
             }
@@ -40,7 +40,7 @@ public class ImageProcessor {
     }
 
 
-    public static BufferedImage convertToBlackAndWhite(BufferedImage colorImage) {
+    public static BufferedImage convertToBlackAndWhite1(BufferedImage colorImage) {
         BufferedImage blackWhiteImage = new BufferedImage(colorImage.getWidth(), colorImage.getHeight(), BufferedImage.TYPE_BYTE_BINARY);
         int width = colorImage.getWidth();
         int height = colorImage.getHeight();
@@ -65,24 +65,82 @@ public class ImageProcessor {
         return blackWhiteImage;
     }
 
-    private static void displayImages(BufferedImage colorImage, BufferedImage blackWhiteImage) {
+    public static BufferedImage convertToBlackAndWhite2(BufferedImage colorImage) {
+        BufferedImage blackWhiteImage = new BufferedImage(colorImage.getWidth(), colorImage.getHeight(), BufferedImage.TYPE_BYTE_BINARY);
+        int width = colorImage.getWidth();
+        int height = colorImage.getHeight();
+
+        for (int y = 0; y < height; y++) {
+            for (int x = 0; x < width; x++) {
+                int pixel = colorImage.getRGB(x, y);
+
+                int red = (pixel >> 16) & 0xff;
+                int green = (pixel >> 8) & 0xff;
+                int blue = pixel & 0xff;
+
+                int gray = (int) (0.1 * red + 0.79 * green + 0.11 * blue);
+                int binary = (gray > 128) ? 255 : 0;
+
+                int newPixel = (binary << 16) | (binary << 8) | binary;
+
+                blackWhiteImage.setRGB(x, y, newPixel);
+            }
+        }
+
+        return blackWhiteImage;
+    }
+    public static BufferedImage convertToBlackAndWhite3(BufferedImage colorImage) {
+        BufferedImage blackWhiteImage = new BufferedImage(colorImage.getWidth(), colorImage.getHeight(), BufferedImage.TYPE_BYTE_BINARY);
+        int width = colorImage.getWidth();
+        int height = colorImage.getHeight();
+
+        for (int y = 0; y < height; y++) {
+            for (int x = 0; x < width; x++) {
+                int pixel = colorImage.getRGB(x, y);
+
+                int red = (pixel >> 16) & 0xff;
+                int green = (pixel >> 8) & 0xff;
+                int blue = pixel & 0xff;
+
+                int gray = (int) (0.1 * red + 0.79 * green + 0.2 * blue);
+                int binary = (gray > 128) ? 255 : 0;
+
+                int newPixel = (binary << 16) | (binary << 8) | binary;
+
+                blackWhiteImage.setRGB(x, y, newPixel);
+            }
+        }
+
+        return blackWhiteImage;
+    }
+
+    private static void displayImages(BufferedImage colorImage, BufferedImage bwImage1, BufferedImage bwImage2, BufferedImage bwImage3) {
         SwingUtilities.invokeLater(() -> {
             JFrame frame = new JFrame("Image Processor");
             frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-            frame.setLayout(new GridLayout(1, 2));
+            frame.setLayout(new BorderLayout());
 
             JPanel originalImagePanel = new JPanel();
             originalImagePanel.add(new JLabel(new ImageIcon(colorImage)));
-            frame.add(originalImagePanel);
+            frame.add(originalImagePanel, BorderLayout.NORTH);
 
-            JPanel processedImagePanel = new JPanel();
-            processedImagePanel.add(new JLabel(new ImageIcon(blackWhiteImage)));
-            frame.add(processedImagePanel);
+            JPanel buttonPanel = new JPanel();
+            buttonPanel.setLayout(new GridLayout(1, 3));
+            buttonPanel.add(new JButton("Преобразование 1"));
+            buttonPanel.add(new JButton("Преобразование 2"));
+            buttonPanel.add(new JButton("Преобразование 3"));
+            frame.add(buttonPanel, BorderLayout.CENTER);
+
+            JPanel resultImagesPanel = new JPanel();
+            resultImagesPanel.setLayout(new GridLayout(1, 3));
+            resultImagesPanel.add(new JLabel(new ImageIcon(bwImage1)));
+            resultImagesPanel.add(new JLabel(new ImageIcon(bwImage2)));
+            resultImagesPanel.add(new JLabel(new ImageIcon(bwImage3)));
+            frame.add(resultImagesPanel, BorderLayout.SOUTH);
 
             frame.pack();
             frame.setLocationRelativeTo(null);
             frame.setVisible(true);
         });
     }
-
 }
--- src/test/java/org/image/ImageProcessorTest.java
+++ src/test/java/org/image/ImageProcessorTest.java
@@ -1,8 +1,5 @@
 package org.image;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-
 import org.junit.jupiter.api.Test;
 
 import java.awt.image.BufferedImage;
@@ -16,12 +13,36 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 public class ImageProcessorTest {
 
     @Test
-    public void testConvertToBlackAndWhite() throws IOException {
-        String inputImagePath = "src/main/resources/img/test_image.jpg";
+    public void testConvertToBlackAndWhite1() throws IOException {
+        String inputImagePath = "src/main/resources/img/image.jpg";
         BufferedImage colorImage = ImageIO.read(new File(inputImagePath));
         assertNotNull(colorImage, "Color image should not be null");
 
-        BufferedImage blackWhiteImage = ImageProcessor.convertToBlackAndWhite(colorImage);
+        BufferedImage blackWhiteImage = ImageProcessor.convertToBlackAndWhite1(colorImage);
+        assertNotNull(blackWhiteImage, "Black and white image should not be null");
+
+        assertEquals(BufferedImage.TYPE_BYTE_BINARY, blackWhiteImage.getType(), "Image type should be BufferedImage.TYPE_BYTE_BINARY");
+    }
+
+    @Test
+    public void testConvertToBlackAndWhite2() throws IOException {
+        String inputImagePath = "src/main/resources/img/image.jpg";
+        BufferedImage colorImage = ImageIO.read(new File(inputImagePath));
+        assertNotNull(colorImage, "Color image should not be null");
+
+        BufferedImage blackWhiteImage = ImageProcessor.convertToBlackAndWhite2(colorImage);
+        assertNotNull(blackWhiteImage, "Black and white image should not be null");
+
+        assertEquals(BufferedImage.TYPE_BYTE_BINARY, blackWhiteImage.getType(), "Image type should be BufferedImage.TYPE_BYTE_BINARY");
+    }
+
+    @Test
+    public void testConvertToBlackAndWhite3() throws IOException {
+        String inputImagePath = "src/main/resources/img/image.jpg";
+        BufferedImage colorImage = ImageIO.read(new File(inputImagePath));
+        assertNotNull(colorImage, "Color image should not be null");
+
+        BufferedImage blackWhiteImage = ImageProcessor.convertToBlackAndWhite3(colorImage);
         assertNotNull(blackWhiteImage, "Black and white image should not be null");
 
         assertEquals(BufferedImage.TYPE_BYTE_BINARY, blackWhiteImage.getType(), "Image type should be BufferedImage.TYPE_BYTE_BINARY");
