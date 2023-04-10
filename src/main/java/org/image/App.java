6c2f43338e2ffdc3c1a66ef273ab56fd84874989
Your Name <you@example.com>
2023-04-10 09:23:26 +0200

    FileChooser alpha 0.1.1

---
+++ src/main/java/org/image/App.java
@@ -0,0 +1,42 @@
+package org.image;
+
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import javax.imageio.ImageIO;
+import javax.swing.*;
+
+public class App {
+
+    public static void main(String[] args) {
+
+        double[] weights1 = {0.35, 0.35, 0.35};
+        double[] weights2 = {0.1, 0.79, 0.11};
+        double[] weights3 = {0.79, 0.11, 0.1};
+
+        JFileChooser fileChooser = new JFileChooser();
+        fileChooser.setDialogTitle("Выберите изображение");
+        fileChooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
+        fileChooser.setAcceptAllFileFilterUsed(false);
+
+        int returnValue = fileChooser.showOpenDialog(null);
+
+        if (returnValue == JFileChooser.APPROVE_OPTION) {
+            File selectedFile = fileChooser.getSelectedFile();
+            try {
+                String inputImagePath = selectedFile.getCanonicalPath();
+
+                BufferedImage colorImage = ImageIO.read(new File(inputImagePath));
+
+                BufferedImage bwImage1 = ImageProcessor.convertToBlackAndWhite(colorImage, weights1);
+                BufferedImage bwImage2 = ImageProcessor.convertToBlackAndWhite(colorImage, weights2);
+                BufferedImage bwImage3 = ImageProcessor.convertToBlackAndWhite(colorImage, weights3);
+
+                ImageDisplay.displayImages(colorImage, bwImage1, bwImage2, bwImage3);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+}
--- src/main/java/org/image/ImageProcessor.java
+++ src/main/java/org/image/ImageProcessor.java
@@ -1,43 +1,9 @@
 package org.image;
 
 import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
-import javax.imageio.ImageIO;
-import javax.swing.*;
 
 public class ImageProcessor {
 
-    public static void main(String[] args) {
-        JFileChooser fileChooser = new JFileChooser();
-        fileChooser.setDialogTitle("Выберите изображение");
-        fileChooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
-        fileChooser.setAcceptAllFileFilterUsed(false);
-
-        int returnValue = fileChooser.showOpenDialog(null);
-
-        if (returnValue == JFileChooser.APPROVE_OPTION) {
-            File selectedFile = fileChooser.getSelectedFile();
-            try {
-                String inputImagePath = selectedFile.getCanonicalPath();
-//                String outputImagePath = inputImagePath.substring(0, inputImagePath.lastIndexOf('.')) + "_bw.jpg";
-
-                BufferedImage colorImage = ImageIO.read(new File(inputImagePath));
-
-                double[] weights1 = {0.35, 0.35, 0.35};
-                double[] weights2 = {0.1, 0.79, 0.11};
-                double[] weights3 = {0.79, 0.11, 0.1};
-
-                BufferedImage bwImage1 = convertToBlackAndWhite(colorImage, weights1);
-                BufferedImage bwImage2 = convertToBlackAndWhite(colorImage, weights2);
-                BufferedImage bwImage3 = convertToBlackAndWhite(colorImage, weights3);
-
-                ImageDisplay.displayImages(colorImage, bwImage1, bwImage2, bwImage3);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-    }
     public static BufferedImage convertToBlackAndWhite(BufferedImage colorImage, double[] weights) {
         BufferedImage blackWhiteImage = new BufferedImage(colorImage.getWidth(), colorImage.getHeight(), BufferedImage.TYPE_BYTE_BINARY);
         int width = colorImage.getWidth();
@@ -62,6 +28,5 @@ public class ImageProcessor {
 
         return blackWhiteImage;
     }
-
 
 }
