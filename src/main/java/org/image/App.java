9ec4599125599b16ecc66209e6cbda00f35ce243
Your Name <you@example.com>
2023-05-04 10:20:59 +0200

    commit 40

--- src/main/java/org/image/App.java
+++ src/main/java/org/image/App.java
@@ -16,13 +16,32 @@ import java.util.stream.Collectors;
 
 import static org.image.Window.decodeBase64ToImage;
 
+/**
+ * The App class serves as the main class for the "Magnet Opener" application, which is responsible for selecting,
+ * reading, decoding, and displaying random images from a specified image directory. The class consists of the following components:
+ * - Constants: LOGGER for logging, IMAGE_DIRECTORY for specifying the image directory, and RANDOM for generating random numbers.
+ * - main method: The entry point of the application, which handles the image processing and display.
+ * - getRandomImagePath method: Selects and returns a random image path from the specified directory.
+ * - logURL method: Logs the provided URL.
+ * - readResourceFileToString method: Reads a resource file and returns its content as a string.
+ * - logSelectedImage method: Logs the path of the selected image.
+ * The application reads image paths from a properties file (img.properties) within the specified directory, decodes Base64-encoded images,
+ * and displays them in a window.
+ */
 public class App {
     private static final Logger LOGGER = Logger.getLogger(App.class.getName());
-
     private static final String IMAGE_DIRECTORY = "/img";
-
     private static final Random RANDOM = new Random();
 
+    /**
+     * The main method serves as the entry point for the "Magnet Opener" application. It performs the following steps:
+     * Logs that the application is running.
+     * Retrieves a random image path using getRandomImagePath() method.
+     * Reads the image file as a Base64-encoded string using readResourceFileToString() method.
+     * Decodes the Base64-encoded string back to a BufferedImage using decodeBase64ToImage() method.
+     * If the BufferedImage is successfully obtained, it logs the selected image path and displays the image in a window using Window.displayImages() method.
+     * If the image decoding fails, an error message is printed to the console.
+     */
     public static void main(String[] args) {
 
         LOGGER.info("App \"Magnet Opener\" running");
@@ -41,6 +60,14 @@ public class App {
 
     }
 
+    /**
+     * This method, getRandomImagePath(), returns a randomly-selected image path from the specified image directory.
+     * It reads the image paths from a properties file (img.properties) within the directory, adding them to a list.
+     * After ensuring the list isn't empty, the method selects a random image path from the list and returns it.
+     *
+     * @return A randomly-selected image path from the specified image directory.
+     * @throws RuntimeException if there is an error reading the image directory or if no images are found in the directory.
+     */
     public static String getRandomImagePath() {
 
         List<String> imagePaths = new ArrayList<>();
@@ -80,7 +107,7 @@ public class App {
      *
      * @return The content of the resource file as a string.
      * @throws IllegalArgumentException If the resource file is not found.
-     * @throws RuntimeException If there's an error reading the resource file.
+     * @throws RuntimeException         If there's an error reading the resource file.
      */
     private static String readResourceFileToString(String imagePath) {
 
@@ -96,7 +123,6 @@ public class App {
             throw new RuntimeException("Failed to read resource file: " + imagePath, e);
         }
     }
-
 
     /**
      * Logs the selected image's path.
