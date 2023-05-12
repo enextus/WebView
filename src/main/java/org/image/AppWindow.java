01e393c6b27010c8282141742e28201961d9f57f
Your Name <you@example.com>
2023-05-12 11:46:11 +0200

    13.5

--- src/main/java/org/image/AppWindow.java
+++ src/main/java/org/image/AppWindow.java
@@ -9,31 +9,27 @@ import java.net.MalformedURLException;
 import java.net.URL;
 
 public class AppWindow {
-    static JLabel numberLabel = new JLabel(Integer.toString(LinkParser.getNumberOfFoundLinks()));
-
+    private static final String FRAME = "MaLO © Magnet Links Opener 2023";
+    private static JLabel numberLabel = new JLabel(Integer.toString(LinkParser.getNumberOfFoundLinks()));
     /**
      * The background color used for the components in the program's GUI.
      * Currently set to black.
      */
     private static final Color BACKGROUND_COLOR = Color.BLACK;
-
     /**
      * The text color used for the components in the program's GUI.
      * Currently set to gold (RGB: 255, 215, 0).
      */
     private static final Color TEXT_COLOR = new Color(255, 215, 0); // Gold
-
     /**
      * The label used to display the original image in the program's GUI.
      */
     private static JLabel originalImageLabel;
-
     /**
      * The text area used to display magnet links in the program's GUI.
      */
-    static JTextArea magnetLinksTextArea;
+    private static JTextArea magnetLinksTextArea;
     private static final JTextField urlField = new JTextField();
-
     // Add a mouse event handler for the urlField input field
     static {
         urlField.addMouseListener(new MouseAdapter() {
@@ -101,7 +97,7 @@ public class AppWindow {
     }
 
     private static JFrame createMainFrame() {
-        JFrame frame = new JFrame("MaLO © Magnet Links Opener 2023");
+        JFrame frame = new JFrame(FRAME);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setLayout(new BorderLayout());
         frame.setResizable(false);
@@ -112,10 +108,8 @@ public class AppWindow {
         JPanel centerPanel = new JPanel();
         centerPanel.setLayout(new BorderLayout());
         centerPanel.setBackground(BACKGROUND_COLOR);
-
         originalImageLabel = new JLabel(new ImageIcon(ImgProcessor.scaleImageForPreview(colorImage)));
         centerPanel.add(originalImageLabel, BorderLayout.CENTER);
-
         JPanel buttonPanel = createButtonPanel();
         originalImageLabel.setLayout(new BorderLayout());
         originalImageLabel.add(buttonPanel, BorderLayout.CENTER);
@@ -127,11 +121,9 @@ public class AppWindow {
         JPanel buttonPanel = new JPanel();
         buttonPanel.setLayout(new BoxLayout(buttonPanel, BoxLayout.Y_AXIS));
         buttonPanel.setOpaque(false);
-
         buttonPanel.add(createNumberPanel());
         buttonPanel.add(Box.createRigidArea(new Dimension(0, 10))); // Add a vertical spacing
         buttonPanel.add(createInputPanel());
-
         return buttonPanel;
     }
 
@@ -139,7 +131,6 @@ public class AppWindow {
         numberLabel = new JLabel(Integer.toString(LinkParser.getNumberOfFoundLinks()));
         numberLabel.setFont(new Font("Arial", Font.PLAIN, 53));
         numberLabel.setForeground(TEXT_COLOR);
-
         JPanel numberPanel = new JPanel();
         numberPanel.setLayout(new BoxLayout(numberPanel, BoxLayout.X_AXIS));
         numberPanel.setOpaque(false);
@@ -150,13 +141,10 @@ public class AppWindow {
 
     private static JPanel createUrlPanel() {
         urlField.setColumns(33);
-
         JPanel urlPanel = new JPanel();
         urlPanel.setLayout(new BoxLayout(urlPanel, BoxLayout.X_AXIS));
-
         urlPanel.add(new JLabel(""));
         urlPanel.add(urlField);
-
         urlPanel.setMaximumSize(new Dimension(300, urlField.getPreferredSize().height));
         urlPanel.setPreferredSize(new Dimension(300, urlField.getPreferredSize().height));
 
@@ -191,29 +179,23 @@ public class AppWindow {
         JPanel inputPanel = new JPanel();
         inputPanel.setLayout(new BoxLayout(inputPanel, BoxLayout.Y_AXIS)); // Changed layout to Y_AXIS for vertical alignment
         inputPanel.setOpaque(false);
-
         // This panel contains the url field and existing buttons
         JPanel topPanel = new JPanel();
         topPanel.setLayout(new BoxLayout(topPanel, BoxLayout.X_AXIS));
         topPanel.setOpaque(false);
-
         topPanel.add(createUrlPanel());
         topPanel.add(Box.createRigidArea(new Dimension(3, 0))); // Add a horizontal spacing
         topPanel.add(createOkButton());
-
         // This panel contains the new search button
         JPanel bottomPanel = new JPanel();
         bottomPanel.setLayout(new BoxLayout(bottomPanel, BoxLayout.X_AXIS));
         bottomPanel.setOpaque(false);
-
         bottomPanel.add(createSearchNewButton());
         bottomPanel.add(Box.createRigidArea(new Dimension(3, 0))); // Add a horizontal spacing
         bottomPanel.add(createSearchTopButton());
         bottomPanel.add(Box.createRigidArea(new Dimension(3, 0))); // Add a horizontal spacing
         bottomPanel.add(createClearButton());
         bottomPanel.add(Box.createRigidArea(new Dimension(3, 0))); // Add a horizontal spacing
-
-
         // Add the panels to the main inputPanel
         inputPanel.add(topPanel);
         inputPanel.add(Box.createRigidArea(new Dimension(0, 10))); // Add a vertical spacing
@@ -238,9 +220,7 @@ public class AppWindow {
         magnetLinksTextArea.setEditable(false);
         magnetLinksTextArea.setForeground(TEXT_COLOR);
         magnetLinksTextArea.setBackground(BACKGROUND_COLOR);
-
         JScrollPane scrollPane = new JScrollPane(magnetLinksTextArea);
-
         JPanel textAreaPanel = new JPanel();
         textAreaPanel.setLayout(new BorderLayout());
         textAreaPanel.add(scrollPane, BorderLayout.NORTH);
@@ -252,7 +232,6 @@ public class AppWindow {
         int delay = 150;
         Timer timer = new Timer(delay, e -> {
             // JLabel numberLabel уже объявлен как поле класса AppWindow
-
             numberLabel.setText(Integer.toString(LinkParser.getNumberOfFoundLinks()));
         });
         timer.start();
